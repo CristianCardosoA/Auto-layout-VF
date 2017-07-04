5f8dd39d51d33be930e4423bc75e2aac05e0b161
Cristian Cardoso <cris52@comunidad.unam.mx>
2017-07-03 22:48:30 -0500

    Example 1.

--- Auto-layout-VF/ViewController.swift
+++ Auto-layout-VF/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let mainContainer = UIView()
+        mainContainer.backgroundColor = UIColor.blue
+        mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(mainContainer)
+        
+        let views = ["mainContainer" : mainContainer]
+        
+        let metrics = ["mainContainerWidth" : self.view.bounds.width]
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[mainContainer]|", options: [], metrics: metrics, views: views))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[mainContainer]|", options: [], metrics: metrics, views: views))
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
